78e76be9953d2d3612eec5bfd1013a4e5df40eb4
AlvaroCordoba-5 <cordobaalvaro16@gmail.com>
2022-07-07 16:12:43 -0300

    updates

--- src/components/Aboutme/Aboutme.jsx
+++ src/components/Aboutme/Aboutme.jsx
@@ -1,18 +1,36 @@
-import React from 'react';
-import './aboutme.css'
+import React from "react";
+import Skills from "../Skills/Skills";
+import { FcDownload } from "react-icons/fc";
+
+import "./aboutme.css";
 
 function Aboutme() {
   return (
-    <div className='aboutme'>
-        <h1 style={{color:'white'}}>About Me</h1>
-        <p style={{color:'white'}}>
-Hi'Im Alvaro 
-
-        </p>
-
-
+    <>
+    <div className="cv">
+      <div className="cvtext">
+      <h1>Download Cv<FcDownload></FcDownload></h1>
+      </div>
     </div>
-  )
+    <div className="aboutme">
+        <div className="aboutskills">
+        <Skills></Skills>
+      </div>
+      <div className="text">
+        <h1 style={{ color: "white" }}>About Me</h1>
+        <p style={{ color: "white", width: "50%", margin: "auto" }}>
+          Hi'Im Alvaro Lorem Ipsum is simply dummy text of the printing and
+          typesetting industry. Lorem Ipsum has been the industry's standard
+          dummy text ever since the 1500s, when an unknown printer took a galley
+          of type and scrambled it to make a type specimen book. It has survived
+          not only five centuries, but also the leap into electronic
+          typesetting, remaining essentially unchanged.
+        </p>
+      </div>
+    
+    </div>
+    </>
+  );
 }
 
-export default Aboutme
+export default Aboutme;
--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -1,10 +1,65 @@
-import React from 'react'
-import './skills.css'
+import React from "react";
+import { IoLogoJavascript } from "react-icons/io";
+import { FaReact, FaHtml5, FaCss3Alt, FaNodeJs } from "react-icons/fa";
+import { SiExpress, SiSequelize, SiPostgresql } from "react-icons/si";
+
+import "./skills.css";
 
 function Skills() {
   return (
-    <div className='skills'></div>
-  )
+    <>
+      <div className="skills">
+        <div>
+          <IoLogoJavascript
+            className="reacticons"
+            style={{ color: "black", backgroundColor: "#ffdf00" }}
+          ></IoLogoJavascript>
+        </div>
+        <div>
+          <FaReact
+            className="reacticons"
+            style={{ color: "#61dafb", backgroundColor: "white" }}
+          ></FaReact>
+        </div>
+        <div>
+          <FaHtml5
+            className="reacticons"
+            style={{ color: "white", backgroundColor: "#ff7816" }}
+          ></FaHtml5>
+        </div>
+        <div>
+          <FaCss3Alt
+            className="reacticons"
+            style={{ color: "white", backgroundColor: "#2196f3" }}
+          ></FaCss3Alt>
+        </div>
+        <div>
+          <FaNodeJs
+            className="reacticons"
+            style={{ color: "#6da75d", backgroundColor: "white" }}
+          ></FaNodeJs>
+        </div>
+        <div>
+          <SiExpress
+            className="reacticons"
+            style={{ color: "white", backgroundColor: "#2e2e2e" }}
+          ></SiExpress>
+        </div>
+        <div>
+          <SiSequelize
+            className="reacticons"
+            style={{ color: "#52b0e7", backgroundColor: "white" }}
+          ></SiSequelize>
+        </div>
+        <div>
+          <SiPostgresql
+            className="reacticons"
+            style={{ color: "white", backgroundColor: "#336791" }}
+          ></SiPostgresql>
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default Skills
+export default Skills;
