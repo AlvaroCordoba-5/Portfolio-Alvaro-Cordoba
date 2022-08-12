a3850cbb47166d4858964ae435b2a2ba4ef5404f
AlvaroCordoba-5 <cordobaalvaro16@gmail.com>
2022-08-12 11:32:38 -0300

    updates

--- src/components/Aboutme/Aboutme.jsx
+++ src/components/Aboutme/Aboutme.jsx
@@ -28,14 +28,11 @@ function Aboutme() {
           <Skills></Skills>
         </div>
         <div className="text">
-          <h1 style={{ color: "white" }}>About Me</h1>
-          <p style={{ color: "white", width: "50%", margin: "auto" }}>
-            Hi'Im Alvaro Lorem Ipsum is simply dummy text of the printing and
-            typesetting industry. Lorem Ipsum has been the industry's standard
-            dummy text ever since the 1500s, when an unknown printer took a
-            galley of type and scrambled it to make a type specimen book. It has
-            survived not only five centuries, but also the leap into electronic
-            typesetting, remaining essentially unchanged.
+          <h1 style={{ color: "white" }}>Acerca de mi</h1>
+          <p style={{ color: "white", margin: "auto" }}>
+            <h4 style={{color:"#06b84e"}}>Hola Mundo!</h4><br></br>
+            Soy FullStack Developer buscando su primer experencia laboral IT.
+
           </p>
         </div>
       </div>
